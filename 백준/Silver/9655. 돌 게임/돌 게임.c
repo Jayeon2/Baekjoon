d83c2e0e5f09a3fef6c949f7bd7dc42844cff413
Jayeon2 <136302329+Jayeon2@users.noreply.github.com>
2023-06-12 16:32:07 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+
+    if (n % 2 == 0) {
+        printf("CY");
+    }
+    else {
+        printf("SK");
+    }
+
+    return 0;
+}
