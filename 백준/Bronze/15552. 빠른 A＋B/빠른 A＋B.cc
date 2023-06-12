e86c02ae3346f55dc13e92bf3b3c24d306a3474b
Jayeon2 <136302329+Jayeon2@users.noreply.github.com>
2023-06-12 16:39:26 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 364 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS // 입력 오류 경고 CRT 표시 억제
+#include <stdio.h> // 기본 입,출력 헤드파일
+
+int main(void) // 매인 함수 선언
+{
+	int num, num1, num2;
+	scanf("%d", &num);
+
+	for (int i = 0; i < num; i++) {
+		scanf("%d %d", &num1, &num2);
+		printf("%d\n", num1 + num2);
+	}
+
+	return 0; // 프로그램 종료
+}
