73ab2b533189c1c6de0c0d85752e4cff48d43484
Jayeon2 <136302329+Jayeon2@users.noreply.github.com>
2023-06-12 16:25:26 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS // 입력 오류 경고 CRT 표시 억제
+#include <stdio.h> // 기본 입,출력 헤드파일
+
+int main(void) // 매인 함수 선언
+{
+    int a, b, max, min, gcd, lcm, temp; // gcd = 최대공약수, lcm = 최소공배수
+
+    scanf("%d %d", &a, &b);
+    max = a;
+    min = b;
+    if (max <= b) {
+        max = b;
+    }
+    if (min >= a) {
+        min = a;
+    }
+    if (max % min == 0) {
+        gcd = min;
+    }
+    else if (max % min != 0) {
+        do {
+            temp = min;
+            min = max % min;
+            max = temp;
+        } while (max % min != 0);
+        gcd = min;
+    }
+    lcm = (a * b) / gcd;
+
+    printf("%d\n", gcd);
+    printf("%d", lcm);
+        
+    return 0; // 프로그램 종료
+}
