4c969845369b661d3ecaa7371fb7c102de30f68e
Jayeon2 <136302329+Jayeon2@users.noreply.github.com>
2023-06-12 16:42:57 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS // 입력 오류 경고 CRT 표시 억제
+#include <stdio.h> // 기본 입,출력 헤드파일
+
+int main(void) // 매인 함수 선언
+{  
+    printf("         ,r'\"7\n");
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r\'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+
+    return 0; // 프로그램 종료
+}
