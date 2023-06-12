ca6ac280e2c3bdd8474d6c7946d9770e923d87e0
Jayeon2 <136302329+Jayeon2@users.noreply.github.com>
2023-06-12 16:43:33 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS // 입력 오류 경고 CRT 표시 억제
+#include <stdio.h> // 기본 입,출력 헤드파일
+
+int main(void) // 매인 함수 선언
+{
+	int num;
+	scanf("%d", &num);
+
+	num /= 4;
+	for (int i = 0; i < num; i++) {
+		printf("long ");
+	}
+	printf("int");
+
+	return 0; // 프로그램 종료
+}
