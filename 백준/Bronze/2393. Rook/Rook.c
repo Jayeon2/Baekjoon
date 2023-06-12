022964ae0ea637185797f2fe147f5b9d792d7f9b
Jayeon2 <136302329+Jayeon2@users.noreply.github.com>
2023-06-12 16:22:19 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS // 입력 오류 경고 CRT 표시 억제
+#include <stdio.h> // 기본 입,출력 헤드파일
+
+int main(void) // 매인 함수 선언
+{
+    printf("  ___  ___  ___\n");
+    printf("  | |__| |__| |\n");
+    printf("  |           |\n");
+    printf("   \\_________/\n");
+    printf("    \\_______/\n");
+    printf("     |     |\n");
+    printf("     |     |\n");
+    printf("     |     |\n");
+    printf("     |     |\n");
+    printf("     |_____|\n");
+    printf("  __/       \\__\n");
+    printf(" /             \\\n");
+    printf("/_______________\\\n");
+
+    return 0; // 프로그램 종료
+}
