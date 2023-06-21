b185157c48a135012d9c15a017cb97656f3a5d89
Jayeon2 <136302329+Jayeon2@users.noreply.github.com>
2023-06-22 05:27:27 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+
+{
+    int n1, k1, n2, k2;
+    scanf("%d %d %d %d", &n1, &k1, &n2, &k2);
+
+    printf("%d", n1 * k1 + n2 * k2);
+
+    return 0;
+}
